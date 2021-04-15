1062deeff45c86bf2f2a522a6bafad1ad2f5ce42
Jeremy <jawtoa@gmail.com>
2021-04-14 22:22:56 -0400

    Changed around robot map and made a drivetrain file

---
+++ West Coast Chassis/src/main/java/frc/robot/DriveTrain.java
@@ -0,0 +1,19 @@
+  
+package frc.robot;
+
+
+
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.util.Units;
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import frc.robot.RobotMap;
+
+public class DriveTrain {
+    
+}

f8f1b7ab9dc68b157224638159ad27bf43fdb74c
Adityaprk <Adityaprk04@gmail.com>
2021-07-08 21:00:56 -0400

    YUPP

--- src/main/java/frc/robot/Shooter.java
+++ src/main/java/frc/robot/Shooter.java
@@ -7,6 +7,8 @@ public class Shooter extends TeleopModule {
     static double upperShooterCoefficient = 1;
     static double lowerShooterCoefficient = 1;
 
+    private double pauseTime = 0;
+
     public Shooter() { 
     }
 
@@ -19,11 +21,20 @@ public class Shooter extends TeleopModule {
     }
 
     public void teleopControl() {
-        Container.getInstance().upperShooter.set(ControlSystems.getInstance().gamepadRightTrigger() * upperShooterCoefficient);
+        Container.getInstance().upperShooter.set(ControlSystems.getInstance().gamepadLeftTrigger() * upperShooterCoefficient);
+        Container.getInstance().lowerShooter.set(ControlSystems.getInstance().gamepadLeftTrigger() * lowerShooterCoefficient);
+        
+        if(ControlSystems.getInstance().gamepadLeftTrigger() > 0.05){
+            pauseTime++;
 
-       // if(ControlSystems.getInstance().manipulatorStickTrigger()) {
-            Container.getInstance().lowerShooter.set(ControlSystems.getInstance().gamepadLeftTrigger() * lowerShooterCoefficient);
-        //} 
+            if(pauseTime > 30){
+                Container.getInstance().transfer2.set(ControlSystems.getInstance().gamepadLeftTrigger() * -1);
+            }
+       }else{
+            pauseTime = 0;
+            Container.getInstance().transfer2.set(0);
+        }
+
         Container.getInstance().shooterAngler.set(ControlSystems.getInstance().mGamepadLeftY());
         SmartDashboard.putNumber("Left Trigger: ", ControlSystems.getInstance().gamepadRightTrigger() * upperShooterCoefficient);
         SmartDashboard.putNumber("Right Trigger: ", ControlSystems.getInstance().gamepadLeftTrigger() * lowerShooterCoefficient);
